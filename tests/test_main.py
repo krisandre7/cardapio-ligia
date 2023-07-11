02843dd889d5211999cddac44e9195b0a4a50775
Jonatas <oliveirajhonatas2016@gmail.com>
2023-07-11 17:29:08 -0400

    Implementação de teste Atualizar Produtos

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,57 +4,26 @@ from cardapio.main import app
 
 client = TestClient(app)
 
+def test_atualizar_produto():
+    client.delete('/')
+    client.post(
+        "/produtos/",
+        json={
+            "nome": "Coca-Cola",
+            "descricao": "Refrigrante de cola",
+            "preco": 7.0,
+            "tipo": 1
+        },
+    )
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Salve Salve Família"}
-    
-def test_create_produto():
-    response = client.delete('/')
-    response = client.post(
+    response = client.put(
         "/produtos/",
         json={
             "nome": "Coca-Cola",
-            "descricao": "Refrigerante de cola",
-            "preco": 5.0,
-            "tipo": 1
-        },
-    )
-    assert response.status_code == 200
-    
-def test_pedir_produto():
-    client.delete('/')
-    client.post(
-        "/produtos/",
-        json={
-            "nome": "Coca-Cola",
-            "descricao": "Refrigerante de cola",
-            "preco": 5.0,
+            "descricao": "Coca Cola Trincando",
+            "preco": 10.0,
             "tipo": 1
         },
     )
     
-    response = client.post("/pedido/Coca-Cola")
     assert response.status_code == 200
-    
-def test_pedir_produto_nao_existe():
-    client.delete('/')
-    response = client.post("/pedido/Coca-Cola")
-    assert response.status_code == 404
-    
-def test_pedir_produto_existente():
-    client.delete('/')
-    client.post(
-        "/produtos/",
-        json={
-            "nome": "Coca-Cola",
-            "descricao": "Refrigerante de cola",
-            "preco": 5.0,
-            "tipo": 1
-        },
-    )
-    client.post("/pedido/Coca-Cola")
-    response = client.post("/pedido/Coca-Cola")
-    assert response.status_code == 400
-    
