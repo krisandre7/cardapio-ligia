73f2b4e08e97baf924a581ac773386b50de846db
Kristhian Aguilar <kris.andre7@gmail.com>
2023-07-12 12:47:26 -0400

    mudei nome de teste

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,12 +4,7 @@ from cardapio.main import app
 
 client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Salve Salve Família"}
-    
-def test_create_produto():
+def test_cadastrar_produto():
     response = client.delete('/')
     response = client.post(
         "/produtos/",
@@ -17,6 +12,30 @@ def test_create_produto():
             "nome": "Coca-Cola",
             "descricao": "Refrigerante de cola",
             "preco": 5.0,
+            "tipo": 1
+        },
+    )
+    
+    assert response.status_code == 200
+    
+def test_atualizar_produto():
+    client.delete('/')
+    client.post(
+        "/produtos/",
+        json={
+            "nome": "Coca-Cola",
+            "descricao": "Refrigrante de cola",
+            "preco": 7.0,
+            "tipo": 1
+        },
+    )
+
+    response = client.put(
+        "/produtos/",
+        json={
+            "nome": "Coca-Cola",
+            "descricao": "Coca Cola Trincando",
+            "preco": 10.0,
             "tipo": 1
         },
     )
